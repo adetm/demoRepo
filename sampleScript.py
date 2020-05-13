707994587baf9edab8a6098921ef4b8a71addeaf
Anna Detmold <adetmold@gmail.com>
2020-05-13 11:55:00 -0700

    puppies

--- sampleScript.py
+++ sampleScript.py
@@ -5,10 +5,11 @@ my_matrix = np.eye(5)
 my_second_matrix = np.array[[1,2,3,4]],
                             [2,3,4,5],
                             [4,0,9,8]
-                            [7,7,7,"hello", "blah"]]
+                            [7,7,7,"hello", "puppies", "blah"]]
                         
 
 print(my_matrix)
 print(my_second_matrix) 
 print("hello")
 print("goodbye")
+print ("puppies")
