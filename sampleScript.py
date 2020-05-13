914b60b0c7116a0e24037d3a7acdb4919bc79e14
Anna Detmold <adetmold@gmail.com>
2020-05-13 11:18:56 -0700

    changed my two matrices

---
+++ sampleScript.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd 
+
+my_matrix = np.eye(5)
+my_second_matrix = np.array[[1,2,3,4]],
+                            [2,3,4,5],
+                            [4,0,9,8]
+                            [7,7,7,8]]
+                        
+
+print(my_matrix)
+print(my_second_matrix) 
+print("hello")
+print("goodbye")
